d26204ac12f3a61595d87b6e57145b68f239ccfd
RyanCrummin <S00253911@atu.ie>
2024-04-20 01:48:26 +0100

    Add project files.

---
+++ BankAccount.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Text;
+using System.Text.Unicode;
+using System.Threading.Tasks;
+
+namespace Week_8_Classes_and_objects_part_4__last_part_
+{
+    internal class BankAccount
+    {
+        
+        private int _accountNumber;
+        private string _customerName;
+        private string _accountType;
+        private string _bic;
+        private decimal _bankBalance;
+        private string _bankPin;
+
+        public static int AccountsRegistered;
+
+        public int AccountNumber
+        {
+            get { return _accountNumber; }
+            set { _accountNumber = value; }
+        }
+        public decimal Balance
+        {
+            get { return _bankBalance; }
+        }
+        public string AccountTypeGetter
+        {
+            get { return _accountType; }
+        }
+        public string AccountTypeSetter
+        {
+            set { _accountType = value; }
+        }
+        public string CustomerNameGetter
+        {
+            get { return _customerName; }
+        }
+        private string CustomerNameSetter
+        {
+            set { _customerName = value; }
+        }
+        public string BicGetter
+        {
+            get { return _bic; }
+        }
+        private string BicSetter
+        {
+            set { _bic = value; }
+        }
+        private string BankPin
+        {
+            get { return _bankPin; }
+            set { _bankPin = value; }
+        }
+        public BankAccount()
+        {
+            AccountsRegistered++;
+
+            AccountNumber = AccountsRegistered;
+        }
+        public BankAccount(string Name, string Type)
+        {
+            string PIN = "";
+
+            #region BankAccount2
+            
+            while (AccountsRegistered == 2 && BankPin.Length != 4)
+            {
+                Console.WriteLine("Your account requires a PIN.\nPlease enter a PIN (Must be 4 characters): ");
+                PIN = Console.ReadLine();
+                while (PIN.Length != 4)
+                {
+                    if (PIN.Length > 4)
+                    {
+                        Console.WriteLine("This PIN is too long, please enter a new one (Must be 4 Characters): ");
+                    }
+                    else if (PIN.Length > 4)
+                    {
+                        Console.WriteLine("This PIN is too short, please enter a new one (Must be 4  Characters): ");
+                    }
+                }
+            }
+            #endregion // PARAMETERS FOR THE 2nd BANK ACCOUNT
+            #region BankAccount3
+            while (AccountsRegistered == 3 && BankPin.Length != 4)
+            {
+                Console.WriteLine("Your account requires a PIN.\nPlease enter a PIN (Must be 4 characters): ");
+                PIN = Console.ReadLine();
+                while (PIN.Length != 4)
+                {
+                    if (PIN.Length > 4)
+                    {
+                        Console.WriteLine("This PIN is too long, please enter a new one (Must be 4 Characters): ");
+                    }
+                    else if (PIN.Length > 4)
+                    {
+                        Console.WriteLine("This PIN is too short, please enter a new one (Must be 4  Characters): ");
+                    }
+                }
+            }
+            #endregion // PARAMETERS FOR THE 3rd Bank Account
+            #region BankAccount4
+            while (AccountsRegistered == 4 && BankPin.Length != 4)
+            {
+                Console.WriteLine("Your account requires a PIN.\nPlease enter a PIN (Must be 4 characters): ");
+                PIN = Console.ReadLine();
+                while (PIN.Length != 4)
+                {
+                    if (PIN.Length > 4)
+                    {
+                        Console.WriteLine("This PIN is too long, please enter a new one (Must be 4 Characters): ");
+                        PIN = Console.ReadLine();
+                    }
+                    else if (PIN.Length > 4)
+                    {
+                        Console.WriteLine("This PIN is too short, please enter a new one (Must be 4  Characters): ");
+                        PIN = Console.ReadLine();
+                    }
+                }
+            }
+            #endregion // PARAMETERS FOR THE 4th BANK ACCOUNT
+
+            CustomerNameSetter = Name;
+            AccountTypeSetter = Type;
+            BicSetter = $"IB-123H" + AccountsRegistered;
+            BankPin = PIN;
+            
+            AccountsRegistered++;
+            AccountNumber = AccountsRegistered;
+            
+        }
+        public decimal LodgeMoney(decimal y)
+        {
+            _bankBalance = Balance + y;
+
+            return _bankBalance;
+
+        }
+        public decimal WithdrawMoney(decimal x)
+        {
+            _bankBalance = Balance - x;
+            return _bankBalance;
+        }
+        public decimal CheckBalance()
+        {
+            Console.WriteLine($"You have {Balance} left in your account.");
+            return Balance;
+        }
+        public void PinChanger()
+        {
+            string pin = "";     
+            
+            Console.Write("Please enter in your New Pin (Must be 4 Characters): ");
+
+            pin = Console.ReadLine();
+
+            while (pin.Length != 4)         // CHECKS TO MAKE SURE THE PIN IS 4 CHARACTERS LONG
+            {
+                if(pin.Length > 4)
+                {
+                    Console.WriteLine("This PIN is too long, please enter a new one (Must be 4 Characters): ");
+                    pin = Console.ReadLine();
+                }   
+
+                else if(pin.Length < 4)
+                {
+                    Console.WriteLine("This PIN is too short, please enter a new one (Must be 4 Characters): ");
+                    pin = Console.ReadLine();
+                }
+            }
+            
+            BankPin = pin;      // SETS THE PIN
+            
+        }
+        public string AccountViewer()
+        {
+            Console.WriteLine($"Name: {CustomerNameGetter}");
+            Console.WriteLine($"Bank PIN: {BankPin}");
+            Console.WriteLine($"Bank BIC: {BicGetter}");
+            Console.WriteLine($"Account Type: {AccountTypeGetter}");
+            return AccountViewer();
+
+        }
+       
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,152 @@
+﻿namespace Week_8_Classes_and_objects_part_4__last_part_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region VARIABLES AND CASE INSENSITVE
+            // VARIABLES
+            string answer = "", name, type = "", bic,pinAnswer="", pin="", depositAnswer = "", answerMenu = "";
+            int choice = ' ';
+            decimal amount = ' ';
+            int i = ' ';
+
+            // CASE INSENSITIVE
+            answer.ToLower();
+            type.ToLower();
+            depositAnswer.ToLower();
+            pinAnswer.ToLower();
+            #endregion
+
+            #region Beginning Inputs
+            // BEGIN INPUTS
+            Console.Write("--------------------------------------------\nWould you like to create a bank account(Y/N): ");
+            answer = Console.ReadLine();
+
+            // HOW MANY BANK ACCOUNTS TO BE CREATED
+            BankAccount[] account = new BankAccount[10000]; // MAX AMOUNT OF ACCOUNTS IS 10000
+                                                            // LOOP TO MAKE ACCOUNTS
+            #endregion
+
+            #region BEGINNING LOOP
+            while (answer != "N")
+            {
+                #region NAME + ACCOUNT TYPE
+                // ENTERING IN NAME DATA
+                Console.Write("--------------------------------------------\nPlease enter in your name: ");
+                name = Console.ReadLine();
+
+                // ADDING LINES FOR STYLE
+                Console.Write("--------------------------------------------\nPlease enter in the type of account you are looking to open: ");
+                type = Console.ReadLine();
+                #endregion
+                // MAKES SURE THE TYPE IS 1 OF THE THREE
+                #region CHECKS TO MAKE SURE INPUT IS OKAY
+                while (type != "Current" && type != "Deposit" && type != "Savings")
+                {
+                    Console.WriteLine("Invalid Account Type (Current, Savings, Deposit). Please enter a new type: ");
+                    type = Console.ReadLine();
+                }
+                #endregion
+
+                // CREATION COMPLETE
+                #region COMPLETION
+                Console.WriteLine("--------------------------------------------\nAccount Creation Process is Complete");
+                account[i] = new BankAccount(name, type);
+                // SETS BANK ACCOUNT DATA
+                #endregion
+                // CREATING PIN PROCESS
+                #region CREATE PIN
+                Console.Write("--------------------------------------------\nWould you like to create a PIN for your account? (Y/N): ");
+                pinAnswer = Console.ReadLine();                                           // DECIDES WHETHER
+
+                if (pinAnswer == "Y")
+                {
+                    account[i].PinChanger();
+                }
+                #endregion
+                // ASK IF WANTS TO LODGE MONEY
+                #region LODGE MONEY
+                
+                Console.Write("--------------------------------------------\nWould you like to lodge money? (Y/N): ");
+                depositAnswer = Console.ReadLine();
+
+                if (depositAnswer == "Y")
+                {
+                    Console.Write("--------------------------------------------\nPlease enter the amount to deposit: ");
+                    amount = decimal.Parse(Console.ReadLine());
+                    account[i].LodgeMoney(amount);                              // GETS ADDED TO the account[i].Balance
+                    Console.WriteLine($"--------------------------------------------\nDeposit of {amount} was succesfull");
+                }
+                #endregion
+                #region ACCOUNT NUMBER AND OPTION TO MAKE NEW ACCOUNT
+                Console.WriteLine($"--------------------------------------------\nYour Bank ID: {account[i].AccountNumber}"); // DISPLAYS BANK ACCOUNT NUMBER
+
+                Console.Write("--------------------------------------------\nWould you like to make another account? (Y/N): "); // OPTION TO MAKE NEW ACCOUNT
+                answer = Console.ReadLine();
+                i++;
+                #endregion
+            }
+            #endregion
+
+            #region Access the Menu
+            Console.Write("--------------------------------------------\nWould you like to access your account? (Y/N): ");            // ACCESS POINT TO THE MENU
+            answerMenu = Console.ReadLine();
+            #endregion
+            #region Menu
+            while (answerMenu != "N")
+            {
+                
+                Console.Write("What is your account ID: ");
+                i = int.Parse(Console.ReadLine());
+
+                Console.WriteLine(account[i].AccountViewer()); // DISPLAYS ACCOUNT INFORMATION FROM BankAccount.cs FILE
+
+                #region Menu Choices + Selections
+                Console.WriteLine($"--------------------------------------------\nPlease Select An Option:\n1. Lodge Money: \n2. Withdraw Money: \n3. Check Balance: \n4. Change Pin: \n5. Exit.");
+                switch (choice)
+                {
+                        case 1:
+                        {
+                            Console.Write("--------------------------------------------\nPlease enter the amount to deposit: ");
+                            amount = decimal.Parse(Console.ReadLine());
+                            account[i].LodgeMoney(amount);                              // GETS ADDED TO the account[i].Balance
+                            Console.WriteLine($"--------------------------------------------\nDeposit of €{amount} was succesfull");
+                            break;
+                        }
+                        case 2:
+                        {
+                            Console.Write("--------------------------------------------\nPlease enter the amount to Withdraw: ");
+                            amount = decimal.Parse(Console.ReadLine());
+                            account[i].WithdrawMoney(amount);                              // GETS DEDUCED FROM the account[i].Balance
+                            Console.WriteLine($"--------------------------------------------\nDeposit of €{amount} was succesfull");
+                            break;
+                        }
+                        case 3:
+                        {
+                            account[i].CheckBalance();          // CHECKS THE BALANCE
+                            break;
+                        }
+                        case 4:
+                        {
+                            account[i].PinChanger();            // CHANGES BANK PIN
+                            break;
+                        }
+                        case 5:
+                        {   
+                            Environment.Exit(0);                // EXITS THE PROGRAMME
+                            break;  
+                        }
+                        
+                }
+                #endregion
+
+
+
+            }
+            #endregion
+
+        }
+   
+    }
+}
